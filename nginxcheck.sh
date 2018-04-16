8f356bb5ec36f9d143f00243cad93d9b0da4f909
tvn2one <tvn2one@gmail.com>
2018-04-16 02:19:23 -0400

    Update nginxcheck.sh

--- nginxcheck.sh
+++ nginxcheck.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+#BASH script to ssh into webserver (tbweb-1) and check if nginx service is running.
+#If it already is, it will print that the service is running, otherwise it will start nginx
 service=nginx
-ssh -i "tvn2one.pem.txt" ec2-user@ec2-34-239-110-162.compute-1.amazonaws.com 'bash -s' < /home/scripts/script.sh
-if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+ssh -i "tvn2one.pem.txt" -t ec2-user@172.31.89.175
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) = 7 ))
 then
-echo "$boo-yah!! the nginx service is running!!!"
+echo "booyah!! $service is running!!!"
 else
 /etc/init.d/$service start
 fi
